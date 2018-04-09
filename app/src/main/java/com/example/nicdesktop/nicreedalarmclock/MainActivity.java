15e8d50b2af8445eaa23120c36dec5fdc7884d12
DESKTOP-20DFT68\Admin <reed.hambrook@gmail.com>
2018-04-08 20:19:07 -0600

    Working alarm

--- app/src/main/java/com/example/nicdesktop/nicreedalarmclock/MainActivity.java
+++ app/src/main/java/com/example/nicdesktop/nicreedalarmclock/MainActivity.java
@@ -6,8 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
@@ -32,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         toglBtnSetAlarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    Log.d("TIME", "" + (timePicker.getCurrentHour() * 60 * 60000 + timePicker.getCurrentMinute() * 60000));
                     alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
                             (timePicker.getCurrentHour() * 60 * 60000 + timePicker.getCurrentMinute() * 60000),
                             AlarmManager.INTERVAL_DAY, puzzleIntent);
@@ -41,6 +38,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
+    }
 }
