78ff53f927ba662f742c94af13513b9ae7cdc598
DESKTOP-20DFT68\Admin <reed.hambrook@gmail.com>
2018-04-11 16:58:37 -0600

    colours added

--- app/src/main/java/com/example/nicdesktop/nicreedalarmclock/MainActivity.java
+++ app/src/main/java/com/example/nicdesktop/nicreedalarmclock/MainActivity.java
@@ -7,36 +7,78 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
     private AlarmManager alarmManager;
     private PendingIntent puzzleIntent;
+    private Intent intent;
     private TimePicker timePicker;
     private ToggleButton toglBtnSetAlarm;
+    private TextView puzzleNum;
+    private SeekBar puzzleNumBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, PuzzleActivity.class);
+        intent = new Intent(this, PuzzleActivity.class);
         setContentView(R.layout.activity_main);
+
         puzzleIntent = PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
         alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
         timePicker = (TimePicker) findViewById(R.id.timePicker);
         toglBtnSetAlarm = (ToggleButton) findViewById(R.id.tgleBtnSetAlarm);
+        puzzleNum = (TextView) (findViewById(R.id.puzzleNum));
+        puzzleNumBar = (SeekBar) (findViewById(R.id.seekBarNumPuzzles));
+
 
         toglBtnSetAlarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
+                    intent.putExtra("numPuzles", Integer.parseInt(puzzleNum.getText().toString()));
                     alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
-                            (timePicker.getCurrentHour() * 60 * 60000 + timePicker.getCurrentMinute() * 60000),
+                            getTimeMilli(timePicker),
                             AlarmManager.INTERVAL_DAY, puzzleIntent);
                 } else {
                     alarmManager.cancel(puzzleIntent);
                 }
             }
         });
+
+        puzzleNumBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                puzzleNum.setText(String.valueOf(progress + 1));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private long getTimeMilli(TimePicker timePicker){
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, timePicker.getCurrentHour());
+        calendar.set(Calendar.MINUTE, timePicker.getCurrentMinute());
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        long millis = calendar.getTimeInMillis();
+
+        return millis;
     }
 }
